536a2042f84cf4cf8ad93a9e5f51f5bf58c936d1
bkim01 <bek318@lehigh.edu>
2015-01-24 17:38:23 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Benedict Kim
+//hw01
+
+//define a class
+public class WelcomeClass{
+
+//main method
+  public static void main (String [] args){
+      
+        System.out.println("  ---------------");
+        System.out.println("  |   WELCOME   |");
+        System.out.println("  ---------------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-b--e--k--3--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I was born and raised in NYC and i have lived");
+        System.out.println("in each of the 5 boroughs at least once.");
+        
+}
+
+}
