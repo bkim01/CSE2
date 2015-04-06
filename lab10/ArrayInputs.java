20e5dff0d63b87904248969acaa98eeb12a0543c
bkim01 <bek318@lehigh.edu>
2015-04-06 18:16:55 +0000

    updated lab10

--- lab10/ArrayInputs.java
+++ lab10/ArrayInputs.java
@@ -9,30 +9,32 @@ public class ArrayInputs{
         boolean check = true;
         System.out.println("Please enter the size of your array");
         int size = scan.nextInt();
+        //declare integer size to be the size of the array
         int[] array = new int[size];
-        while(check){
-            for(int i = 0; i<size; i++){
+        while(check){   //continuous loop until broken
+            for(int i = 0; i<size; i++){    //for loop to assign values in empty cells
                 System.out.println("Please enter a positive integer");
-                if(scan.hasNextInt()){
-                    int x = scan.nextInt();
-                    if(x>0){
-                        array[i] = x;
+                if(scan.hasNextInt()){  //if the input is an integer
+                    int x = scan.nextInt(); //assign x to be the integer
+                    if(x>0){    //if it is positive
+                        array[i] = x;   //assign a cell in the array the value of x
                     }
-                    else{
+                    else{   //error message
                         System.out.println("Not a positive integer");
-                        i--;
+                        i--;    //run the loop one more time
                     }
                 }
-                else{
-                    System.out.println("Not an integer");
+                else{   //if the input is not an integer
+                    System.out.println("Not an integer");   //error message
                     i--;
-                    scan.next();
+                    scan.next();    //redeclare scanner
                 }
             }
-            if(array.length == size){
+            if(array.length == size){   //if array is populated then break out of loop
                 check = false;
             }
         }
+        //print out array
         System.out.println("Here is your array");
         for(int i=0; i<size; i++){
             System.out.println(array[i]);
